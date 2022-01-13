c3e69354c74c59b5e7a75a25da2f10d06731575e
uldalex <uldalex@gmail.com>
2022-01-13 22:30:25 +0300

    +2 таба

--- src/blocks/splide/splide.js
+++ src/blocks/splide/splide.js
@@ -77,6 +77,48 @@ document.addEventListener( 'DOMContentLoaded', function() {
         }); 
         splide.mount();
         }
+        if(document.getElementById("press-relise")) {
+          var banner =  document.getElementById('press-relise') 
+         
+          var splide = new Splide( banner, {
+            type   : 'loop',
+            perPage: 1,
+            perMove: 1,
+            autoplay:false,
+            pauseOnHover:true,
+            pauseOnFocus: true,
+            arrows:true,
+          }); 
+          splide.mount();
+          }
+          if(document.getElementById("comments")) {
+            var banner =  document.getElementById('comments') 
+           
+            var splide = new Splide( banner, {
+              type   : 'loop',
+              perPage: 1,
+              perMove: 1,
+              autoplay:false,
+              pauseOnHover:true,
+              pauseOnFocus: true,
+              arrows:true,
+            }); 
+            splide.mount();
+            }
+            if(document.getElementById("articles")) {
+              var banner =  document.getElementById('articles') 
+             
+              var splide = new Splide( banner, {
+                type   : 'loop',
+                perPage: 1,
+                perMove: 1,
+                autoplay:false,
+                pauseOnHover:true,
+                pauseOnFocus: true,
+                arrows:true,
+              }); 
+              splide.mount();
+              }
         if(document.getElementById("analitic")) {
           var banner =  document.getElementById('analitic') 
          
